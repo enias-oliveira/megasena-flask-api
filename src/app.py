556b91bc2a083276d514fa3eb83c71ffc701559d
Enias Oliveira <eniasoliveira27@gmail.com>
2021-04-14 01:02:47 -0300

    wip: adds JWT authentication config

--- src/app.py
+++ src/app.py
@@ -4,9 +4,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from src.configurations import database, migration, serializers
+    from src.configurations import database, migration, serializers, authentication
     from src import configurations, views
 
+    authentication.init_app(app)
     configurations.init_app(app)
     database.init_app(app)
     migration.init_app(app)
