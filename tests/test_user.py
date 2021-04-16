12ac26a6a8e4c715fc901099796f75e873c7aace
Enias Oliveira <eniasoliveira27@gmail.com>
2021-04-16 07:35:17 -0300

    test(wip): test user login reponse and jwt token

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,12 @@
-import pytest
+from pytest import fixture
+
+from flask_jwt_extended import decode_token
 
 from src.models.user_model import UserModel
 from src.app import create_app
 
 
-@pytest.fixture
+@fixture(scope="module")
 def test_app():
     app = create_app()
     app.app_context().push()
@@ -16,29 +18,32 @@ def test_app():
     app.db.drop_all()
 
 
-@pytest.fixture
+@fixture(scope="module")
 def test_client(test_app):
     return test_app.test_client()
 
 
-def test_standard_user_creation(test_client):
-    given = {
+@fixture
+def user_body():
+    return {
         "name": "ioasys",
         "email": "ioasys@mail.com",
         "password": "b4ckEnd",
     }
 
+
+def test_standard_user_creation(test_client, user_body):
     expected_response = {
         "id": 1,
         "name": "ioasys",
         "email": "ioasys@mail.com",
     }
-    expected_user_name = given["name"]
-    expected_user_email = given["email"]
+    expected_user_name = user_body["name"]
+    expected_user_email = user_body["email"]
 
     actual_response = test_client.post(
         "/api/users/signup",
-        json=given,
+        json=user_body,
     ).get_json()
 
     actual_user = UserModel.query.get(1)
@@ -48,3 +53,18 @@ def test_standard_user_creation(test_client):
     assert actual_response == expected_response
     assert actual_user_name == expected_user_name
     assert actual_user_email == expected_user_email
+
+
+def test_user_login(test_client, user_body):
+    user_body.pop("name")
+
+    expected_jwt_id = 1
+    expected_response_status = 201
+
+    actual_response = test_client.post("/api/users/login", json=user_body)
+    actual_status = actual_response.status_code
+    actual_response_token = actual_response.get_json().get("access_token")
+    actual_jwt_id = decode_token(actual_response_token).get("sub")
+
+    assert expected_response_status == actual_status
+    assert expected_jwt_id == actual_jwt_id
