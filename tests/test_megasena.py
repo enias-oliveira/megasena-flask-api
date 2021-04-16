f683ac6223f392ff9b526aa28f67a9ac1f3ee4f2
Enias Oliveira <eniasoliveira27@gmail.com>
2021-04-16 08:48:37 -0300

    test: adds test for megasena draw and ticket list

--- tests/test_megasena.py
+++ tests/test_megasena.py
@@ -4,7 +4,7 @@ from src.models.ticket_model import TicketModel
 from src.app import create_app
 
 
-@fixture(scope="module")
+@fixture()
 def test_app():
     app = create_app()
     app.app_context().push()
@@ -16,12 +16,12 @@ def test_app():
     app.db.drop_all()
 
 
-@fixture(scope="module")
+@fixture()
 def test_client(test_app):
     return test_app.test_client()
 
 
-@fixture(scope="module")
+@fixture()
 def access_token(test_client):
     user_body = {"name": "megasena", "email": "megasena@mail.com", "password": "Mega"}
 
@@ -58,3 +58,55 @@ def test_create_ticket(test_client, access_token):
     assert actual_status_code == expected_status_code
     assert actual_ticket_id == expected_ticket_id
     assert actual_numbers_length == expected_numbers_length
+
+
+def test_last_megasena_draw(test_client, access_token):
+    expected_status_code = 200
+    expected_draw_numbers = [3, 20, 22, 32, 35, 50]
+
+    actual_response = test_client.get(
+        "/api/megasenas/draws",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+
+    actual_status_code = actual_response.status_code
+    actual_numbers = actual_response.get_json().get("latest_draw")
+
+    assert actual_status_code == expected_status_code
+    assert actual_numbers == expected_draw_numbers
+
+
+def test_list_tickets(test_client, access_token):
+    body = {"numbers": 8}
+
+    first_ticket = (
+        test_client.post(
+            "/api/megasenas",
+            headers={"Authorization": f"Bearer {access_token}"},
+            json=body,
+        )
+        .get_json()
+        .get("ticket_numbers")
+    )
+
+    second_ticket = (
+        test_client.post(
+            "/api/megasenas",
+            headers={"Authorization": f"Bearer {access_token}"},
+            json=body,
+        )
+        .get_json()
+        .get("ticket_numbers")
+    )
+
+    megasenas_response = (
+        test_client.get(
+            "/api/megasenas",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        .get_json()
+        .get("megasena_tickets")
+    )
+
+    assert megasenas_response[1]["ticket_numbers"] == first_ticket
+    assert megasenas_response[0]["ticket_numbers"] == second_ticket
