c792744991766ce2169e1b033dda89593b7f854a
Enias Oliveira <eniasoliveira27@gmail.com>
2021-04-16 07:09:21 -0300

    test(fix): removes unecessary imports

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,9 +1,7 @@
 import pytest
-from flask_testing import TestCase
 
 from src.models.user_model import UserModel
 from src.app import create_app
-from src.configurations.database import db
 
 
 @pytest.fixture
@@ -30,11 +28,23 @@ def test_standard_user_creation(test_client):
         "password": "b4ckEnd",
     }
 
-    expected_response = {"id": 1, "name": "ioasys", "email": "ioasys@mail.com"}
-    expected_user_name = "ioasys"
+    expected_response = {
+        "id": 1,
+        "name": "ioasys",
+        "email": "ioasys@mail.com",
+    }
+    expected_user_name = given["name"]
+    expected_user_email = given["email"]
 
-    actual_response = test_client.post("/api/users/signup", json=given).get_json()
-    actual_user_name = UserModel.query.get(1).name
+    actual_response = test_client.post(
+        "/api/users/signup",
+        json=given,
+    ).get_json()
+
+    actual_user = UserModel.query.get(1)
+    actual_user_name = actual_user.name
+    actual_user_email = actual_user.email
 
     assert actual_response == expected_response
     assert actual_user_name == expected_user_name
+    assert actual_user_email == expected_user_email
